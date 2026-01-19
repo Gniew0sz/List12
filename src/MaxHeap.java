430304f4ecc19dca6540f1d3d7c38fe2451a8158
miesz <mieszalontko0@o2.pl>
2026-01-19 11:49:54 +0100

    heap sort added

--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -4,10 +4,11 @@ public class MaxHeap {
      * This constructor turns a randomly ordered array into a max heap
      * @param arr the table to be transformed into a max heap
     */
+    private int[] arr;
     public MaxHeap(int[] arr) {
         int n =arr.length;
         for (int i=n/2-1;i>=0;i--) heapify(arr,n,i); //according to the formula for indices of an element in a heap the last element that has children is n/2-1 so there is no point in starting the heapifying process on a different index
-
+        this.arr=arr;
     }
 
     /**
@@ -16,7 +17,7 @@ public class MaxHeap {
      * @param n the length/last index of the heap
      * @param i the index of the node
      */
-    public void heapify(int[] arr, int n, int i){
+    private void heapify(int[] arr, int n, int i){
         int largest = i;
         int left = 2 * i + 1;
         int right = 2 * i + 2;
@@ -46,5 +47,18 @@ public class MaxHeap {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
+    }
+
+    /**
+     * Turns the max heap into a sorted array
+     * @return the sorted array
+     */
+    public int[] heapSort(){
+        int n = arr.length;
+        for (int i=n-1;i>0;i--) {
+            swap(arr,i,0);
+            heapify(arr, i, 0);
+        }
+        return arr;
     }
 }
